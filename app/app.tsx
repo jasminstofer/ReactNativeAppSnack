48086cc87f975a677efe716d0b8220d54d383175
Jasmin Stofer <jasmin.stofer@matriq.ch>
2025-05-17 16:22:51 +0200

    Add App because snack needs that?

---
+++ app/app.tsx
@@ -0,0 +1,10 @@
+// App.tsx
+import { Text, View } from 'react-native';
+
+export default function App() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Hello from Snack-compatible version!</Text>
+        </View>
+    );
+}
